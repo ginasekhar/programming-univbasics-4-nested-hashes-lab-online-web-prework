8475e2657fe491a76ef6252a17e220a8ea6d3b66
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-20 11:25:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -116,7 +116,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay] [:known_for] = 'GUI'
+     programmer_hash[:alan_kay][:known_for] = 'GUI'
      
      
 end
@@ -138,5 +138,5 @@ def adding_to_dennis
         }
      }
     dennis_ritchies_language = 
-  programmer_hash[:dennis_ritchie][:languages] =  "Assembly"
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 end
