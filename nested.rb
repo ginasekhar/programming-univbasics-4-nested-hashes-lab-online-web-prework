9ac70b0ac5a57781e8c5c0e02c26d38f4b5074ff
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-20 11:22:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,6 +137,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  
-  programmer_hash[:dennis_ritchie][:languages]   "Assembly"
+    dennis_ritchies_language
+  programmer_hash[:dennis_ritchie][:languages] =  "Assembly"
 end
